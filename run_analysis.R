cdf6867c07c928527284056a65fcffa026ef9af5
Yvonne Flory <yvflory@gmail.com>
2015-02-19 12:41:08 +0000

    final version

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,11 @@
 run_analysis <- function() {
     
+    require(dplyr)
+    
     ## read in variable names
     variables <- read.table("UCI HAR Dataset/features.txt")
     var_vector <- as.vector(variables[,2])
-        
+    
     ## read in test tables
     test_subject <- read.table("UCI HAR Dataset/test/subject_test.txt")
     test_data <- read.table("UCI HAR Dataset/test/X_test.txt")
@@ -16,7 +18,7 @@ run_analysis <- function() {
     
     ## extract only variables that contain mean and std values
     test_means <- test_data[,grepl("mean|std", colnames(test_data))]
-
+    
     ## combine all test files
     test_all <- cbind(test_subject, test_activity, test_means)
     
@@ -35,48 +37,32 @@ run_analysis <- function() {
     
     ## combine all test files
     train_all <- cbind(train_subject, train_activity, train_means)
-
+    
     ## combine the two test and training datasets
     data_all <- merge(test_all, train_all, all = TRUE)
     
-    ## extract mean for each variable by activity and subject
-    ## 
-    ## iterate over each subject, and over each activity
-    ## for each combination, calculate mean for each variable
-    ## add to new df: subject - activity - means...
-    mean_data <- data.frame(matrix(ncol = ncol(data_all), nrow = 180))
-    counter <- 1
-
-    for (i in 1:30) { ## subjects
-        for (j in 1:6) { ## activities
-            temp_df <- filter(data_all, subject == i, activity == j)
-            mean_data[counter,1] <- i
-            mean_data[counter,2] <- j
-            ## iterate over all (measured) variables and assign the mean
-            ## of each column to the new dataframe
-            for (k in 3:ncol(data_all)) {
-                mean_data[counter,k] <- mean(temp_df[1:nrow(temp_df),k])
-            }
-            counter <- counter + 1
-        }  
-    }
-    ## add column names to new dataset
-    colnames(mean_data) <- colnames(data_all)
+    data_filtered <- group_by(data_all,subject,activity)
+    
+    ## Calculate means for each subject and activity
+    means <- summarise_each(data_filtered,funs(mean))
     
     ## turn activity variable into factors with descriptive levels
-    mean_data$activity <- factor(mean_data$activity)
-    levels(mean_data$activity) <- list("Walking" = "1",
-                                      "Walking upstairs" = "2", 
-                                      "Walking downstairs" = "3",
-                                      "Sitting" = "4",
-                                      "Standing" = "5",
-                                      "Lying" = "6")
+    means$activity <- factor(means$activity)
+    levels(means$activity) <- list("Walking" = "1",
+                                       "Walking upstairs" = "2", 
+                                       "Walking downstairs" = "3",
+                                       "Sitting" = "4",
+                                       "Standing" = "5",
+                                       "Lying" = "6")
     
     ## no renaming of variables necessary, as names are taken from features.txt
     ## As far as I could see, the erroneous duplicate column names were
     ## among those that were dropped.
     
     ## write final table into text file
-    write.table(mean_data, file = "tidy_data_set.txt", row.names = FALSE)
-
+    write.table(means, file = "tidy_data_set.txt", row.names = FALSE)
+    
+    
+    
+    
 }
